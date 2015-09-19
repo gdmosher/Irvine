c22002b4e73a0fd0d8ef5f3a89bc9aa139bd3b84
gdmosher <gdmosher@calgro.com>
2015-09-19 03:27:35 -0700

    First work on MSI-GT70
    Use .sln files to start new projects.

--- Examples/ch03/AddTwoSum_64.asm
+++ Examples/ch03/AddTwoSum_64.asm
@@ -2,16 +2,26 @@
 
 ExitProcess proto
 
+COMMENT ~
+;OUTPUT MODEL ISSUE NOT 64BIT (remember to set config)
+;INCLUDE irvine64.inc (does not exist)
+~
 .data
 sum qword 0
 
 .code
 main proc
+	mov	  rax,12345678AABBFF03h
+	push rax
+	pop rax
 	mov	  rax,5
 	add	  rax,6
 	mov   sum,rax
 
-	mov   ecx,0
+	mov   ecx,0	;or rcx
+	mov   edx,0	;reminder 64 bit windows api params in 4 registers
+	mov   r8,0
+	mov   r9,0
 	call  ExitProcess
 main endp
 end
--- Examples/ch03/AddVariables.asm
+++ Examples/ch03/AddVariables.asm
@@ -9,10 +9,14 @@ ExitProcess proto,dwExitCode:dword
 firstval  dword 20002000h
 secondval dword 11111111h
 thirdval  dword 22222222h
+fourthval dword 0000FF03h
 sum dword 0
 
 .code
 main proc
+	mov eax,fourthval
+	push eax
+	pop eax
 	mov	eax,firstval				
 	add	eax,secondval		
 	add eax,thirdval
