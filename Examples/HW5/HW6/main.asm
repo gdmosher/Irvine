0f55ce4c9516991257a3a4022d49afd2f50a4f0b
gdmosher <gdmosher@calgro.com>
2015-12-15 20:52:05 -0800

    Commit HW6 in solution HW5

---
+++ Examples/HW5/HW6/main.asm
@@ -0,0 +1,107 @@
+TITLE MASM Template						(main.asm)
+;=================================================
+; ATTACHED: HW6
+;	
+;=================================================
+; Program Description:	HW6
+; Author:				GORDON MOSHER
+; Date Created:			2015-1215
+; Last Modification:	2015-1215
+;=================================================
+
+INCLUDE Irvine32.inc
+
+
+.data
+myMessage BYTE "HW6",0dh,0ah,0
+
+
+.code
+GetNums PROC
+	push ebp
+	mov ebp, esp
+	mov edx, OFFSET msg0
+	call WriteString
+	call Crlf
+	mov esi, [ebp+8]			;OFFSET numbers
+	mov ecx, [ebp+12]			;LENGTHOF numbers
+nextNum:
+	mov edx, OFFSET msg1
+	call WriteString
+	call ReadDec
+	mov [esi], eax
+	add esi, TYPE numbers
+	loop nextNum
+	pop ebp
+	ret
+GetNums ENDP
+ShowNums PROC
+	push ebp
+	mov ebp, esp
+	mov edx, OFFSET msg2
+	call WriteString
+	call Crlf
+	mov esi, [ebp+8]			;OFFSET numbers
+	mov ecx, [ebp+12]			;LENGTHOF numbers
+nextNum2:
+	mov eax, [esi]
+	call WriteDec
+	call Crlf
+	add esi, TYPE numbers
+	loop nextNum2
+	pop ebp
+	ret
+ShowNums ENDP
+main PROC
+	call Clrscr  
+
+	mov	 edx,offset myMessage
+	call WriteString
+	call CrLf
+
+;	HW6
+	.data
+	msg0	BYTE	"You will enter 5 numbers",0
+	msg1	BYTE	"Enter a number:  ",0
+	msg2	BYTE	"Here are the numbers: ",0
+	numbers	DWORD	5 dup(0)
+
+	.code
+	push LENGTHOF numbers
+	push OFFSET numbers
+	call GetNums
+;	call Clrscr
+	push LENGTHOF numbers
+	push OFFSET numbers
+	call ShowNums
+	call CrLf
+	call Crlf
+
+
+	call DumpRegs
+
+	jmp done
+
+done:
+	call WaitMsg
+	exit
+main ENDP
+
+END main
+COMMENT~
+PROGRAM OUTPUT
+Here are the numbers:
+1
+2
+3
+4
+5
+
+
+
+  EAX=00000005  EBX=003F0000  ECX=00000000  EDX=00406031
+  ESI=0040605C  EDI=00401005  EBP=0019FF94  ESP=0019FF74
+  EIP=00403496  EFL=00000202  CF=0  SF=0  ZF=0  OF=0  AF=0  PF=0
+
+Press any key to continue...
+~
